e5aec4a66e427130cf82156d9fa474f506df56fa
choppaah <124285024+choppaah@users.noreply.github.com>
2023-02-03 11:49:35 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   env     = "${local.env}"
 }
 
-module "http_server" {
+module "http_serve2r" {
   source  = "../../modules/http_server"
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
